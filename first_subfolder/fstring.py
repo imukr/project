52b327d57c2d908747f470287f7a474e481167ba
imukr <i0979887766@gmail.com>
2022-11-08 12:52:44 +0200

    08112022

---
+++ first_subfolder/fstring.py
@@ -0,0 +1,8 @@
+# num_1 = 80
+# num_2 = 97
+# print (f'simple example: {num_1} + {num_2} = {num_1 + num_2}')
+
+
+name = input('Enter your name:')
+last_name = input('Enter your last name:')
+print (f"Your fool name:{name} {last_name}")
